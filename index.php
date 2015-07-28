506f45c5d30769a76cf67797841a336fcf5e3362
Dan Vera <dan@yovu.co>
2015-07-28 01:46:20 -0400

    init

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Recursiv</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+  <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+</head>
+<body>
+  <canvas id="bg"></canvas>
+
+  <div id="content">
+    <div id="head" class="columns columns-2">
+      <h1 id="phoboslab-logo">&Lambda;|Recursiv</h1>
+    </div>
+    
+    <h2 id="intro">
+    </h2>
+  </div>
+
+  <a class="scroll-tip" href="mailto:contact@recursiv" style="top: inherit; bottom: 22px;">contact@recursiv.co</a>
+
+  <script type="text/javascript" src="assets/js/jquery-1.9.1.min.js"></script>
+  <script type="text/javascript" src="assets/js/page.js"></script>
+</body>
+</html>
